891ee1f7080f793e129f3d1c4cb35e579365ae34
Swanand Buva <swanandev@gmail.com>
2021-06-28 17:51:02 +0530

    Modified Click Sound!

--- JS/landing.js
+++ JS/landing.js
@@ -2,10 +2,12 @@ const startGameAnchor = document.getElementById('start-game');
 const handleSelect = (level) => {
     startGameAnchor.href = "./home.html?" + level;
     console.log(startGameAnchor.href);
+    pressButton();
 }
 
 const nameNameElement = document.getElementById('player-name');
 const savePlayerName = () => {
+    pressButton();
     let name = nameNameElement.value.trim();
     window.localStorage.setItem("currentPlayer", name == "" ? "Guest": name);
 }
